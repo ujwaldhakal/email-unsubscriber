bf93593f3ac3062e98d42c497c03a56d8f5c6339
Ujwal Dhakal <ujwal.dhakal@werkspot.nl>
2022-01-25 22:57:13 +0100

    Added easy makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
 .PHONY: *
 
-parse-email:
+dockerup:
+	docker-compose up
+
+publish:
+	docker-compose run unsubscriber go run main.go sync-inbox
+
+consume:
 	docker-compose run unsubscriber go run main.go parse-email
+
+asyncConsume:
+	make -j consume
---
+++ cmd/hello-world.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "sync-inbox",
+	Short: "Hugo is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
+                love by spf13 and friends in Go.
+                Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+
+		fmt.Println("hello world")
+
+	},
+}
+
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
